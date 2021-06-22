69b7cc954b284905986179bbf293cc4cbdce9325
Shivani Arorra <79195342+arorrashivani@users.noreply.github.com>
2021-06-22 14:56:43 +0530

    Solution

---
+++ index.js
@@ -0,0 +1,21 @@
+const https = require('https');
+
+https.get('https://coderbyte.com/api/challenges/json/age-counting', (resp) => {
+  
+  let data = '';
+
+  resp.on('data', (d) => {
+    data += [d]
+  })
+
+  resp.on('end', () => {
+    let parsedData = data.split(',')
+                .filter(data => !data.indexOf(' age='))
+                .map(data => data.replace(" age=", ''))
+                .map(data => parseInt(data))
+                .filter(data => {
+                  return data >= 50
+                }).length
+    console.log(parsedData)
+  })
+});
